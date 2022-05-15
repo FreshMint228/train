f44389271ebaca1d4262ff616de3a377f023dc1a
Danya <skywalker1228228@gmail.com>
2022-05-15 10:42:18 +0300

    js update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,68 @@
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
-console.log(numberOfFilms);
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+   while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+   }
+}
+
+start();
+
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {}, 
    actors: {},
    genres: [],
-   privat: false
-};
-const a = prompt("Какой последний фильм вы посмотрели?");
-const b = prompt("На сколько бы вы его оценили?");
-const c = prompt("Какой последний фильм вы посмотрели?");
-const d = prompt("На сколько бы вы его оценили?");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+   privat: false   
+}; 
+
+function rememberMyFilms() {
+   for(let i = 0; i < 2; i++){
+      const a = prompt("Какой последний фильм вы посмотрели?"),
+         b = prompt("На сколько бы вы его оценили?");
+   
+      if (a != null && b != null && a != "" && b != "" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+      } else {
+       i--;
+      }
+   
+   }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+   if (personalMovieDB.count <= 10){
+      console.log ('Просмотренно слишком мало фильмов.');
+   } else if (personalMovieDB.count < 30){
+      console.log ('Вы классический зритель.');
+   } else if (personalMovieDB.count > 29){
+      console.log ('Вы киноман.');
+   } else {
+      console.log ('Возникла ошибка.');
+   }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+   if(personalMovieDB.privat == false){
+      console.log(personalMovieDB);
+   } else{
+      console.log("Ошибка");
+   }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+   for (let i = 1; i <= 3; i++){
+      const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+      personalMovieDB.genres[i - 1] = genre;
+   }
+}
+
+writeYourGenres();
